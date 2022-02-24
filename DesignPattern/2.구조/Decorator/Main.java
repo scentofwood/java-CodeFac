cf145aaa77fdbb4855275a243ee17455ddc97bc0
scentofwood <61110506+scentofwood@users.noreply.github.com>
2022-02-24 23:53:27 +0900

    디자인패턴 Decorator

---
+++ DesignPattern/2.구조/Decorator/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Display b1 = new StringDisplay("Hello world.");
+        Display b2 = new SideBorder(b1, '#');
+        Display b3 = new FullBorder(b2);
+        b1.show();
+        System.out.println("");
+        b2.show();
+        System.out.println("");
+        b3.show();
+        System.out.println("");
+        Display b4 =
+            new SideBorder(
+                new FullBorder(
+                    new FullBorder(
+                        new SideBorder(
+                            new FullBorder(
+                                new StringDisplay("æ»≥Á«œººø‰")
+                            ),
+                            '*'
+                        )
+                    )
+                ),
+                '/'
+            );
+        b4.show();
+    }
+}
